b08e91c2cdfe890548d573571f694a6bab0bab1e
lhcarabajal <lhcarabajal@gmail.com>
2022-09-21 14:31:20 -0300

    Feito funções que altera cor dos botões com click

--- js/index.js
+++ js/index.js
@@ -16,13 +16,58 @@ let minutes = Number(displayMinutes.textContent);
 let seconds = Number(displaySeconds.textContent);
 let timeTimeOut;
 
-let buttonMusic = document.querySelector(".card");
-let icon = document.querySelector(".card svg path");
+let iconChuva = document.querySelector(".chuva svg path");
+let iconFloresta = document.querySelector(".floresta svg path");
+let iconLareira = document.querySelector(".lareira svg path");
+let iconLanchonete = document.querySelector(".lanchonete svg path");
 
-// função para alterar a cor bo buttom no click da música
-function mudarCorButtons() {
-  buttonMusic.style.backgroundColor = "#02799d";
-  icon.style.fill = "white";
+// funções para alterar a cor dos botões e icone com o click
+function buttonColorFloresta() {
+  soundFloresta.style.backgroundColor = "#02799d";
+  soundChuva.style.backgroundColor = "#E1E1E6";
+  soundLanchonete.style.backgroundColor = "#E1E1E6";
+  soundLareira.style.backgroundColor = "#E1E1E6";
+
+  iconFloresta.style.fill = "white";
+  iconChuva.style.fill = "#323238";
+  iconLanchonete.style.fill = "#323238";
+  iconLareira.style.fill = "#323238";
+}
+
+function buttonColorChuva() {
+  soundChuva.style.backgroundColor = "#02799d";
+  soundFloresta.style.backgroundColor = "#E1E1E6";
+  soundLanchonete.style.backgroundColor = "#E1E1E6";
+  soundLareira.style.backgroundColor = "#E1E1E6";
+
+  iconChuva.style.fill = "white";
+  iconFloresta.style.fill = "#323238";
+  iconLanchonete.style.fill = "#323238";
+  iconLareira.style.fill = "#323238";
+}
+
+function buttonColorLanchonete() {
+  soundLanchonete.style.backgroundColor = "#02799d";
+  soundFloresta.style.backgroundColor = "#E1E1E6";
+  soundChuva.style.backgroundColor = "#E1E1E6";
+  soundLareira.style.backgroundColor = "#E1E1E6";
+
+  iconLanchonete.style.fill = "white";
+  iconChuva.style.fill = "#323238";
+  iconFloresta.style.fill = "#323238";
+  iconLareira.style.fill = "#323238";
+}
+
+function buttonColorLareira() {
+  soundLareira.style.backgroundColor = "#02799d";
+  soundFloresta.style.backgroundColor = "#E1E1E6";
+  soundChuva.style.backgroundColor = "#E1E1E6";
+  soundLanchonete.style.backgroundColor = "#E1E1E6";
+
+  iconLareira.style.fill = "white";
+  iconChuva.style.fill = "#323238";
+  iconLanchonete.style.fill = "#323238";
+  iconFloresta.style.fill = "#323238";
 }
 
 // Função para resetar o botão play quando o cronometro zerar e resetar o display
@@ -84,7 +129,7 @@ soundFloresta.addEventListener("click", function () {
   sound.soundLanchonete.pause();
   sound.soundLareira.pause();
 
-  mudarCorButtons();
+  buttonColorFloresta();
 });
 
 soundChuva.addEventListener("click", function () {
@@ -93,7 +138,7 @@ soundChuva.addEventListener("click", function () {
   sound.soundLanchonete.pause();
   sound.soundLareira.pause();
 
-  mudarCorButtons();
+  buttonColorChuva();
 });
 
 soundLanchonete.addEventListener("click", function () {
@@ -102,7 +147,7 @@ soundLanchonete.addEventListener("click", function () {
   sound.soundFloresta.pause();
   sound.soundLareira.pause();
 
-  mudarCorButtons();
+  buttonColorLanchonete();
 });
 
 soundLareira.addEventListener("click", function () {
@@ -111,5 +156,5 @@ soundLareira.addEventListener("click", function () {
   sound.soundLanchonete.pause();
   sound.soundChuva.pause();
 
-  mudarCorButtons();
+  buttonColorLareira();
 });
